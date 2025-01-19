a09dcce5e1b6783c4893dc790159f212815ffe18
Turbovv <giga.elizbarashvili14@gmail.com>
2025-01-19 13:30:34 +0400

    add: formatDate

--- src/app/guest-book/page.tsx
+++ src/app/guest-book/page.tsx
@@ -2,11 +2,11 @@
 import SignIn from "~/components/sign-in";
 import { useState } from "react";
 import { api } from "~/trpc/react";
+import { formatDate } from '~/lib//format';
 
 export default function GuestBook() {
   const [content, setContent] = useState("");
   const [loading, setLoading] = useState(false);
-  
 
   const createCommentMutation = api.comments.createComment.useMutation({
     onSuccess: () => {
@@ -39,20 +39,25 @@ export default function GuestBook() {
   if (isError) return <div>Error loading comments</div>;
 
   return (
-    <div className="px-4 py-1">
-      <div className="flex justify-between items-center">
-        <SignIn />
-        <form onSubmit={handleSubmit} className="w-10/12">
+    <div className="px-4 py-1 custom-scroll"
+      style={{
+        maxHeight: "600px",
+        overflowY: "auto",
+      }}
+    >
+      <div className="flex justify-between items-center border">
+        <SignIn />:
+        <form onSubmit={handleSubmit} className="border w-full max-w-6xl">
           <div className="flex items-center justify-between">
             <textarea
               value={content}
               onChange={(e) => setContent(e.target.value)}
               placeholder="Leave a message"
               required
-              className="bg-gray-900 w-full resize-none h-6 focus:outline-none focus:border-transparent"
+              className="bg-gray-900  max-w-2xl resize-none h-6 focus:outline-none focus:border-transparent"
             />
             <button
-              className="px-10 bg-gray-400 text-gray-800 flex items-center justify-center"
+              className="px-14 bg-gray-400 text-gray-800 flex items-center justify-center"
               type="submit"
               disabled={loading}
             >
@@ -68,10 +73,11 @@ export default function GuestBook() {
 
       <div>
         {comments && comments.length > 0 ? (
-          comments.map((comment) => (
-            <div className="flex gap-2" key={comment.id}>
-              <p className="lowercase">{comment.createdByName} :</p>
-              <p>{comment.content}</p>
+          [...comments].reverse().map((comment) => (
+            <div className="flex gap-2 items-start justify-between" key={comment.id}>
+              <p className="lowercase">{comment.createdByName}</p>:
+              <p className="w-full max-w-5xl break-words">{comment.content}</p>
+              <p>{formatDate(comment.createdAt)}</p>
             </div>
           ))
         ) : (
---
+++ src/lib/format.ts
@@ -0,0 +1,12 @@
+export const formatDate = (date: Date) => {
+    const options: Intl.DateTimeFormatOptions = {
+      month: '2-digit',
+      day: '2-digit',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: true,
+    };
+  
+    return new Date(date).toLocaleString('en-US', options);
+};
