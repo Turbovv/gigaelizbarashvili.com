ce126492c300ae3be3b04a408820f86f92f9e1b3
Turbovv <giga.elizbarashvili14@gmail.com>
2025-02-17 19:35:11 +0400

    fix: locale issue with /projects paths

--- src/app/[locale]/projects/[id]/page.tsx
+++ src/app/[locale]/projects/[id]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
-import { useParams } from "next/navigation";
+
+import { useParams, usePathname, useRouter } from "next/navigation";
 import { api } from "~/trpc/react";
-import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { ArrowLeft } from "lucide-react";
 import { ClipLoader } from "react-spinners";
@@ -9,41 +9,48 @@ import { ClipLoader } from "react-spinners";
 export default function StackDetailsPage() {
   const router = useRouter();
   const { id } = useParams();
-  const projectid = Number(id);
+  const pathname = usePathname();
 
+  const projectid = Number(id);
   const { data: project, isLoading } = api.projects.getProjectById.useQuery({
     id: projectid,
   });
 
-  if (isLoading) return <div className="flex justify-center items-center h-20"><ClipLoader color="#5de4c7" /></div>;
+  const locale = pathname.split("/")[1] || "en";
+
+  if (isLoading)
+    return (
+      <div className="flex justify-center items-center h-20">
+        <ClipLoader color="#5de4c7" />
+      </div>
+    );
 
   return (
-    <>
-
-      <div
-        style={{
-          maxHeight: "700px",
-        }}
-        className="flex justify-center items-center p-4 custom-scroll"
-      >
-        {project && (
-          <div className="max-w-6xl h-[600px] rounded-lg shadow-md p-6 flex flex-col space-y-4">
-               <button onClick={() => history.back()}>
-            <ArrowLeft />
+    <div
+      style={{
+        maxHeight: "700px",
+      }}
+      className="flex justify-center items-center p-4 custom-scroll"
+    >
+      {project && (
+        <div className="max-w-6xl h-[600px] rounded-lg shadow-md p-6 flex flex-col space-y-4">
+          <button onClick={() => router.push(`/${locale}/projects`)} className="flex items-center gap-2 text-gray-400 hover:text-gray-200 transition-all">
+            <ArrowLeft /> Back
           </button>
-            <img
-              src={project.url}
-              alt={project.name}
-              className="w-full h-auto rounded-lg"
-            />
-              <h2 className="text-2xl font-semibold text-gray-400">{project.name}</h2>
-              <p className="text-gray-400 py-4">{project.desc}
-              <Link className="underline px-2 text-gray-200" href={project.websitelink}>Preview.</Link>
-              <Link className="underline text-gray-200" href={project.githublink}>Source Code.</Link>
-              </p>
-          </div>
-        )}
-      </div>
-    </>
+
+          <img
+            src={project.url}
+            alt={project.name}
+            className="w-full h-auto rounded-lg"
+          />
+          <h2 className="text-2xl font-semibold text-gray-400">{project.name}</h2>
+          <p className="text-gray-400 py-4">
+            {project.desc}
+            <Link className="underline px-2 text-gray-200" href={project.websitelink}>Preview.</Link>
+            <Link className="underline text-gray-200" href={project.githublink}>Source Code.</Link>
+          </p>
+        </div>
+      )}
+    </div>
   );
 }
--- src/app/[locale]/projects/page.tsx
+++ src/app/[locale]/projects/page.tsx
@@ -1,11 +1,31 @@
 "use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 import Link from "next/link";
 import { api } from "~/trpc/react";
 import { ClipLoader } from "react-spinners";
+import { usePathname } from "next/navigation";
 
 export default function Page() {
-  const { data: projectsData, isLoading, } = api.projects.getProjects.useQuery();
-  if (isLoading) return <div className="flex justify-center items-center h-20"><ClipLoader color="#5de4c7" /></div>;
+  const { data: projectsData, isLoading } = api.projects.getProjects.useQuery();
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const locale = pathname.split("/")[1] || "en";
+
+  useEffect(() => {
+    if (pathname === "/projects") {
+      router.replace(`/${locale}/projects`);
+    }
+  }, [pathname, locale, router]);
+
+  if (isLoading)
+    return (
+      <div className="flex justify-center items-center h-20">
+        <ClipLoader color="#5de4c7" />
+      </div>
+    );
 
   return (
     <div
@@ -23,49 +43,49 @@ export default function Page() {
         }}
       >
         {projectsData?.map((project) => (
-          <Link key={project.id} href={`/projects/${project.id}`}>
-          <div
-          style={{
-            backgroundColor: "#252525",
-            borderRadius: "10px",
-            boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
-            transition: "transform 0.3s, box-shadow 0.3s",
-          }}
-        >
-          <img
-            src={project.url}
-            alt={project.name}
-            style={{
-              width: "100%",
-              height: "200px",
-              borderRadius: "8px",
-            }}
-          />
-          <div className="p-2">
-          <h2
-            style={{
-              fontSize: "1.5rem",
-              color: "#add7ff",
-            }}
-          >
-            {project.name}
-          </h2>
-          <p
-            style={{
-              fontSize: "1rem",
-              color: "#ccc",
-              overflow: "hidden",
-              display: "-webkit-box",
-              WebkitLineClamp: 3,
-              WebkitBoxOrient: "vertical",
-              lineHeight: "1.6",
-            }}
-          >
-            {project.desc}
-          </p>
-          </div>
-        </div>
-        </Link>
+          <Link key={project.id} href={`/${locale}/projects/${project.id}`}>
+            <div
+              style={{
+                backgroundColor: "#252525",
+                borderRadius: "10px",
+                boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
+                transition: "transform 0.3s, box-shadow 0.3s",
+              }}
+            >
+              <img
+                src={project.url}
+                alt={project.name}
+                style={{
+                  width: "100%",
+                  height: "200px",
+                  borderRadius: "8px",
+                }}
+              />
+              <div className="p-2">
+                <h2
+                  style={{
+                    fontSize: "1.5rem",
+                    color: "#add7ff",
+                  }}
+                >
+                  {project.name}
+                </h2>
+                <p
+                  style={{
+                    fontSize: "1rem",
+                    color: "#ccc",
+                    overflow: "hidden",
+                    display: "-webkit-box",
+                    WebkitLineClamp: 3,
+                    WebkitBoxOrient: "vertical",
+                    lineHeight: "1.6",
+                  }}
+                >
+                  {project.desc}
+                </p>
+              </div>
+            </div>
+          </Link>
         ))}
       </div>
     </div>
