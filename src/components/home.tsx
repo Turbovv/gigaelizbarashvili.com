221a7f8ce685a0ba86b866374107237fb416870f
Turbovv <giga.elizbarashvili14@gmail.com>
2025-08-21 21:40:48 +0400

    fix: navbar responsive

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -104,27 +104,37 @@ const Terminal: React.FC<TerminalProps> = ({ children }) => {
   return (
     <div
       ref={terminalRef}
-      className={`fixed z-10 flex justify-center items-center ${
-        size.width <= 1024 ? "w-full h-full" : ""
-      }`}
+      className={`fixed z-10 flex justify-center items-center
+        w-full h-full
+        left-0 top-0
+        transition-all duration-200
+      `}
       style={{
+        ...(size.width > 1024
+          ? {
         left: position.x,
         top: position.y,
         width: `${size.width}px`,
         height: `${size.height}px`,
-        transition: "all 0.2s ease",
+            }
+          : {}),
       }}
     >
-      <div className="relative bg-gradient-to-tr from-black to-neutral-800 text-gray-300 lg:rounded-lg shadow-lg border border-gray-700 h-full w-full max-w-full max-h-full">
-        <div className=" flex items-center px-4 py-2">
+      <div className={`relative bg-gradient-to-tr from-black to-neutral-800 text-gray-300 shadow-lg border border-gray-700
+          w-full h-full
+          max-w-[1400px] max-h-[800px]
+          sm:rounded-lg
+          sm:p-4 p-2
+          flex flex-col
+        `}
+      >
+        <div className="flex items-center sm:px-4 px-2 sm:py-2 py-2">
           <Header onFullscreenToggle={toggleFullscreen} onClose={resetTerminal} />
           <LanguageSwitcher />
         </div>
-
-        <div className="h-[88%] overflow-auto">
+        <div className="flex-1 overflow-auto">
           {children}
         </div>
-
         <Footer />
       </div>
     </div>
